8cdaeff35c41da238c28c28ef5db1099a429dba2
9nnative <66473604+9nnative@users.noreply.github.com>
2020-06-15 16:36:14 +0200

    push5

---
+++ exo11.php
@@ -0,0 +1,13 @@
+<?php 
+
+/* Initialiser un tableau de x marques de voitures. Ecrire un algorithme permettant de parcourir ce tableau et d’en afficher le contenu (une marque de voiture par ligne). Il est également demandé d’afficher le nombre de marques de voitures présentes dans le tableau.
+Exemple : tableau ➔ « Peugeot », « Renault », « BMW », « Mercedes » Affichage (attention à utiliser une liste à puces)
+Il y a 4 marques de voitures dans le tableau :
+Peugeot
+Renault
+BMW
+Mercedes 
+
+!! Installer Laragon !!
+*/
+
