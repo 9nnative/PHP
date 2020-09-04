ddcfd7880cbb88a2a7868c5b5dd758ada3bd7bd5
Corentin <66473604+9nnative@users.noreply.github.com>
2020-09-04 10:33:24 +0200

    Push 16
    background blur

--- Exercices/Forum/ForumMVC/view/forum/home.php
+++ Exercices/Forum/ForumMVC/view/forum/home.php
@@ -6,9 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-    <section class="mainsection">
+    <div class="uk-background-blend-exclusion">
 
 
-    </section>
+    </div>
 </body>
 </html>
